87b69583c0161c824231f28258645d5663187d28
Alex Blackmore <ablackmore113@gmail.com>
2024-01-16 14:24:24 +1100

    remove pycache from repo, implement login route (without database elements)

--- auth/views.py
+++ auth/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, JsonResponse
 import requests
 import os
+from base64 import b64encode
 from dotenv import load_dotenv
-from helpers import get_access_token
 
 load_dotenv()
 REDIRECT_URI = "http://localhost:5173/callback";
 GRANT_TYPE = "authorization_code"
 ACCESS_URL = "https://accounts.spotify.com/api"
 ACCESS_TOKEN = "/token"
-
+EXPECTED_SCOPE = "streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public"
 # Create your views here.
 def login(request: HttpRequest):
     try:
-        access_code = request.GET['access_code']
+        print(request.GET)
+        access_code = request.GET["access_code"]
     except KeyError:
-        return HttpResponse("No access code", status=400)
-    access_token = get_access_token(os.environ["client_id"], os.environ["client_secret"])
-    headers = {"Authorization": "Bearer " + access_token, "content-type": "application/x-www-form-urlencoded"}
+        return JsonResponse({"error": "No access code"}, status=400)
+    encoded_details = b64encode(bytes(os.environ["CLIENT_ID"] + ":" + os.environ["CLIENT_SECRET"], encoding="utf-8")).decode("ascii")
+    headers = {"Authorization": "Basic " + encoded_details, "Content-Type": "application/x-www-form-urlencoded"}
     body = {"code": access_code, "grant_type": GRANT_TYPE, "redirect_uri": REDIRECT_URI}
     response = requests.post(ACCESS_URL + ACCESS_TOKEN, body, headers=headers)
-    
+    if response.status_code != 200:
+        return JsonResponse({"error": "Bad access code"}, status=400)
+    result = response.json()
+    if sorted(result["scope"].split()) != sorted(EXPECTED_SCOPE.split()):
+        return JsonResponse({"error": "Could not authenticate with expected scope"}, status=400)
+    user_access_token = result["access_token"]
+    refresh_token = result["refresh_token"]
+    response = JsonResponse({"access_token": user_access_token}, status=200)
+    # update database with refresh token etc... TODO
+    return response
+
